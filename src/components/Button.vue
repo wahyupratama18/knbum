207e1cbf91d530ef62a093834da5db9d7d19bb66
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2023-01-18 18:13:09 +0700

    Ready to show

--- src/components/Button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <button class="inline-block px-6 py-2.5 bg-sky-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-800 active:shadow-lg transition duration-150 ease-in-out">
-        <slot />
-    </button>
-</template>
--- src/components/ToBe.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-    <h4 class="bg-sky-500 text-white p-4 rounded-lg shadow-lg">
-        <slot />
-    </h4>
-</template>
